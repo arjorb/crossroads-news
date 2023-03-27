fa8711413472edc7d6554b2d03ef155dbc2b57dd
John U <arjorb@gmail.com>
2023-03-27 11:16:15 +0200

    setting up the environment

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+
+const App = () => {
+  return <h1 className='text-3xl font-bold flex justify-center text-[#017977]'>Hello Crossroads News!</h1>;
+};
+
+export default App;
