6ccf45382d65814a6c05ff6cccb1be19a22d859c
John U <arjorb@gmail.com>
2023-03-28 10:12:38 +0200

    creating ui folder to keep the small reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import Layout from './components/Layout/Layout';
-import Navbar from './components/Layout/Navbar';
+import Layout from './components/layout/Layout';
+import Navbar from './components/layout/Navbar';
 import Cards from './components/Cards';
 
 const App = () => {
