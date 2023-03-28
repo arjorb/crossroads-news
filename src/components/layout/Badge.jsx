4f201c6389d2b3284134014569965bd0fd0e64d0
John U <arjorb@gmail.com>
2023-03-28 11:31:03 +0200

    add the control on the badge component

--- src/components/layout/Badge.jsx
+++ src/components/layout/Badge.jsx
@@ -1,6 +1,9 @@
+import { HiOutlineChevronRight, HiOutlineChevronLeft } from "react-icons/hi";
 import BadgeItem from '../ui/BadgeItem';
 const Badge = () => {
   return (
+    <div className="flex items-center gap-4">
+    <div><HiOutlineChevronLeft/></div>
     <div className='flex gap-4  overflow-x-auto'>
         <BadgeItem name='Gaming' />
         <BadgeItem name='User Interface Design' />
@@ -26,6 +29,8 @@ const Badge = () => {
         <BadgeItem name='comedy' />
         <BadgeItem name='New to you' />
         <BadgeItem name='Apple' />
+      </div>
+      <div><HiOutlineChevronRight/></div>
       </div>
   )
 }
