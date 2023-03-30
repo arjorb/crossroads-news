da1762a534c931d69c8d3b02dd140126f3c7e4e6
John U <arjorb@gmail.com>
2023-03-30 08:56:26 +0200

    refactoring the publisher component

--- src/components/publisher/Publishers.jsx
+++ src/components/publisher/Publishers.jsx
@@ -1,13 +1,36 @@
+import { useRef } from "react";
 import { HiOutlineChevronRight, HiOutlineChevronLeft } from "react-icons/hi";
 import PublisherItem from './PublisherItem';
 const Publishers = () => {
+
+  const publishRef = useRef(null);
+  const boundaryRef = useRef(null);
+
+  const prevSlide = () =>{
+    publishRef.current.scrollBy({
+      left:-300,
+      behavior: "smooth"
+    })
+    
+  }
+
+  const nextSlide = () =>{
+    publishRef.current.scrollBy({
+      left:300,
+      behavior: "smooth"
+    })
+
+  }
+ 
+  console.log(publishRef.current);
+
   return (
     <div className="flex items-center gap-1">
-    <div className="hover:bg-[#F6F6F7] p-2.5 rounded-full cursor-pointer"><HiOutlineChevronLeft/></div>
-    <div className='flex gap-4  overflow-x-auto'>
-      {['Gaming','User Interface Design','Chill-out music','comedy','New to you','New to you','Recently uploaded','comedy','New to you','New to you','Recently uploaded'].map((item,index) => <PublisherItem key={index} item={item} />)}
+    <button onClick={prevSlide} className="hover:bg-[#F6F6F7] p-2.5 rounded-full cursor-pointer"><HiOutlineChevronLeft/></button>
+    <div ref={publishRef} className='flex gap-4  overflow-x-auto'>
+      {['Gaming','User Interface Design','Chill-out music','comedy','New to you','New to you','Recently uploaded','comedy','New to you','New to you','Recently uploaded'].map((item,index) => <PublisherItem key={index} item={item}/>)}
       </div>
-      <div className="hover:bg-[#F6F6F7] p-2.5 rounded-full cursor-pointer"><HiOutlineChevronRight/></div>
+      <button onClick={nextSlide} className="hover:bg-[#F6F6F7] p-2.5 rounded-full cursor-pointer"><HiOutlineChevronRight/></button>
       </div>
   )
 }
