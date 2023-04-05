4cc94f597ccf5f03e20696282eabfb885ad04123
John U <arjorb@gmail.com>
2023-04-05 16:39:03 +0200

    remove the SearchItem Component and refactoring my searchModal component

--- src/components/search/SearchModal.jsx
+++ src/components/search/SearchModal.jsx
@@ -7,7 +7,6 @@ import SearchItem from "./SearchItem";
 
 const SearchModal = ({ search, close }) => {
   const [input, setInput] = useState("");
-  const [filtered, setFiltered] = useState([]);
 
   const articles = useSelector((state) => state.articles.value);
 
@@ -21,35 +20,21 @@ const SearchModal = ({ search, close }) => {
     handleFilter(input);
   };
 
-  console.log(input);
-  console.log(filtered);
   return (
     <>
       {search && (
-        <div id="overlay" onClick={close} className="fixed top-0 left-0  w-full h-screen bg-black/70 flex justify-center pt-20 z-50">
-          <div className="relative bg-[#F6F6F7] w-5/12 h-m[30vh] rounded-md p-5 overflow-hidden">
+        <div id="overlay" onClick={close} className="fixed top-0 left-0  w-full h-screen bg-black/50 flex justify-center pt-20 z-50">
+          <div className="relative bg-[#F6F6F7] w-5/12 h-[20vh] rounded-md p-5 overflow-hidden">
             <form action="">
-              <div className="bg-white w-full border text-2xl  border-indigo-400 rounded-sm flex items-center gap-2">
+              <div className="bg-white w-full border text-2xl border-indigo-400 rounded-sm flex items-center gap-2">
                 <BiSearch size={30} className="ml-2 text-indigo-400" />
                 <input type="text" value={input} onChange={handleInput} placeholder="Search News" className="w-full h-14 outline-none" />
               </div>
             </form>
-            <div className="">
-              {filtered.length < 1 ? (
-                <p className="text-center mt-3 text-[#888] text-[12px]">No recent searches</p>
-              ) : (
-                filtered.map((item, index) => <SearchItem key={index} {...item} />)
-              )}
-            </div>
             <div className="absolute bottom-0 left-0 w-full h-10 bg-white shadow-black shadow-2xl px-5 py-3 text-sm flex items-center justify-between text-[#B4B4B7]">
               <div className="flex items-center gap-2 text-sm">
                 <BsArrowReturnLeft />
                 to enter
-              </div>
-              <div className="flex items-center gap-2">
-                <HiArrowSmDown />
-                <HiArrowSmUp />
-                <p className="text-sm">to navigate</p>
               </div>
               <div className="flex gap-2">
                 <p className="text-sm">Esc</p>
