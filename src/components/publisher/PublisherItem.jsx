50210bb1333e0dbb7674e5a3b9f979644337304b
John U <arjorb@gmail.com>
2023-04-08 11:20:34 +0200

    change the color

--- src/components/publisher/PublisherItem.jsx
+++ src/components/publisher/PublisherItem.jsx
@@ -3,7 +3,7 @@ const PublisherItem = ({ name, id, active, handleActive }) => {
     <div
       onClick={handleActive}
       className={`px-3 py-2 min-w-max text-[#494949] text-sm rounded-md cursor-pointer  ${
-        active === id ? "bg-[#797979] text-white" : "bg-[#F6F6F7] hover:bg-[#e9e9e9]"
+        active === id ? "bg-[#797977] text-white" : "bg-[#F6F6F7] hover:bg-[#e9e9e9]"
       }`}
     >
       {name}
