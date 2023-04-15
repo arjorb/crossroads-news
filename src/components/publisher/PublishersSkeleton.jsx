41db9034d2f16e45823b0ac7f0115fedea2b2dbd
John U <arjorb@gmail.com>
2023-04-15 21:08:31 +0200

    adding the array for the bagde for the publishers

--- src/components/publisher/PublishersSkeleton.jsx
+++ src/components/publisher/PublishersSkeleton.jsx
@@ -1,8 +1,14 @@
 const PublishersSkeleton = () => {
   return (
-    <div role="status" className=" max-w-full animate-pulse">
-      <div className="h-8 bg-gray-200 rounded-md dark:bg-gray-200 w-[985px] mb-4"></div>
-    </div>
+    <>
+      {Array(10)
+        .fill(0)
+        .map((_, index) => (
+          <div role="status" className=" max-w-full animate-pulse">
+            <div className="h-8 bg-gray-200 rounded-md dark:bg-gray-200 w-20 mb-4"></div>
+          </div>
+        ))}
+    </>
   );
 };
 
