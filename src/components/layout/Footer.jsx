f21baf3abde473d4030600e26e3f9c577a5adba0
John U <arjorb@gmail.com>
2023-04-15 18:26:32 +0200

    adding the copyright and the year

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div className="text-center my-5">Footer</div>;
+  return <div className=" text-sm text-center text-[#5a5a5a] my-5">All rights reserved © John U. {new Date().getFullYear()}</div>;
 };
 
 export default Footer;
