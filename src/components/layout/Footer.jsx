3fcfed4c9adf43432080d384010c6554c78785ac
John U <arjorb@gmail.com>
2023-04-15 22:03:43 +0200

    refactoring the footer component

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div className=" text-sm text-center text-[#7b7b7b] my-7">All rights reserved © John U. {new Date().getFullYear()}</div>;
+  return <div className="text-sm font-medium text-center text-[#7b7b7b] my-7">All rights reserved © John U. {new Date().getFullYear()}</div>;
 };
 
 export default Footer;
