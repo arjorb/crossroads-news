122c19690f202b8a50495c760dc866584976852c
John U <arjorb@gmail.com>
2023-04-15 21:52:07 +0200

    done working on the PublisherSkeleton component

--- src/components/publisher/PublishersSkeleton.jsx
+++ src/components/publisher/PublishersSkeleton.jsx
@@ -1,14 +1,14 @@
 const PublishersSkeleton = () => {
   return (
-    <>
+    <div className="flex gap-4 animate-pulse">
+      <div className="bg-gray-200 w-8 h-8 rounded-full"></div>
       {Array(10)
         .fill(0)
         .map((_, index) => (
-          <div role="status" className=" max-w-full animate-pulse">
-            <div className="h-8 bg-gray-200 rounded-md dark:bg-gray-200 w-20 mb-4"></div>
-          </div>
+          <div key={index} className="h-8 bg-gray-200 rounded-md w-20 mb-4"></div>
         ))}
-    </>
+      <div className="bg-gray-200 w-8 h-8 rounded-full"></div>
+    </div>
   );
 };
 
