2ee81455bb10eb1dbca481d2786ef5f5cc412ea1
John U <arjorb@gmail.com>
2023-04-15 19:13:10 +0200

    change the color of the text on the footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div className=" text-sm text-center text-[#5a5a5a] my-5">All rights reserved © John U. {new Date().getFullYear()}</div>;
+  return <div className=" text-sm text-center text-[#7b7b7b] my-7">All rights reserved © John U. {new Date().getFullYear()}</div>;
 };
 
 export default Footer;
