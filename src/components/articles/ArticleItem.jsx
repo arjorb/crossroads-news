73414852b9edcc78e3843ff9d8170db1b1d2e43a
John U <arjorb@gmail.com>
2023-04-17 17:07:18 +0200

    change the transtion to scale the image beacuse backdrop is causing the problems

--- src/components/articles/ArticleItem.jsx
+++ src/components/articles/ArticleItem.jsx
@@ -1,11 +1,8 @@
 const ArticleItem = ({ urlToImage, title, publishedAt, handleSingleArticle }) => {
   return (
-    <div
-      onClick={handleSingleArticle}
-      className="mt-16 w-[295px] h-52 bg-[#f0faff] rounded-md overflow-hidden relative cursor-pointer hover:translate-y-3 duration-300"
-    >
-      <img src={urlToImage} alt="" className="w-full object-cover" />
-      <div className="backdrop-blur-3xl bg-white/3 absolute bottom-0 w-full rounded-md px-2 py-1 text-sm">
+    <div onClick={handleSingleArticle} className="mt-16 w-[295px] h-52 bg-[#f0faff] rounded-md overflow-hidden relative cursor-pointer">
+      <img src={urlToImage} alt="urlImage" className="w-full object-cover hover:scale-110 duration-300" />
+      <div className="backdrop-blur-3xl absolute bottom-0 w-full rounded-md px-2 py-1 text-sm">
         <h1 className="font-semibold">{title.substring(0, 60)}...</h1>
         <p className="mt-2 text-[12px] font-medium">{publishedAt}</p>
       </div>
