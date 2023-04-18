a8951791b7d7ff864cd7241231a5854d8cb2f2b6
John U <arjorb@gmail.com>
2023-04-18 19:57:58 +0200

    adding the dark class on the PublishersSkeleton

--- src/components/publisher/PublishersSkeleton.jsx
+++ src/components/publisher/PublishersSkeleton.jsx
@@ -1,13 +1,13 @@
 const PublishersSkeleton = () => {
   return (
     <div className="flex gap-4 animate-pulse">
-      <div className="bg-gray-200 w-8 h-8 rounded-full"></div>
+      <div className="bg-gray-200 dark:bg-[#222222] w-8 h-8 rounded-full"></div>
       {Array(10)
         .fill(0)
         .map((_, index) => (
-          <div key={index} className="h-8 bg-gray-200 rounded-md w-[74px] mb-4"></div>
+          <div key={index} className="h-8 bg-gray-200 dark:bg-[#222222] rounded-md w-[74px] mb-4"></div>
         ))}
-      <div className="bg-gray-200 w-8 h-8 rounded-full"></div>
+      <div className="bg-gray-200 dark:bg-[#222222] w-8 h-8 rounded-full"></div>
     </div>
   );
 };
