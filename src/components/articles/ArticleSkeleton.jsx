586eae7d206aac671ac5c2570ed7cb6b544725a0
John U <arjorb@gmail.com>
2023-04-18 20:04:15 +0200

    refactoring the ArticleSkeleton component

--- src/components/articles/ArticleSkeleton.jsx
+++ src/components/articles/ArticleSkeleton.jsx
@@ -5,10 +5,10 @@ const ArticleSkeleton = () => {
         .fill(0)
         .map((_, index) => (
           <div key={index} role="status" className="rounded-md relative flex gap-x-12 flex-wrap animate-pulse">
-            <div className="relative mt-16 w-[295px] h-52 rounded-md bg-gray-200">
-              <div className="bg-gray-300 absolute bottom-0 w-full rounded-md px-2 text-sm">
-                <div className="h-2.5 bg-gray-200 rounded-md w-full my-5"></div>
-                <div className="h-2 bg-gray-200 rounded-md w-full mb-2"></div>
+            <div className="relative mt-16 w-[295px] h-52 rounded-md bg-gray-200 dark:bg-[#222222]">
+              <div className="bg-gray-300 dark:bg-[#333] absolute bottom-0 w-full rounded-md px-2 text-sm">
+                <div className="h-2.5 bg-gray-200 dark:bg-[#222222] rounded-md w-full my-5"></div>
+                <div className="h-2 bg-gray-200 dark:bg-[#222222] rounded-md w-full mb-2"></div>
               </div>
             </div>
           </div>
