ba0c63b68c09b10203d180aa7047aaa8a301f444
John U <arjorb@gmail.com>
2023-04-19 08:42:31 +0200

    adding the duration on the change of the theme

--- src/components/layout/ThemeBtn.jsx
+++ src/components/layout/ThemeBtn.jsx
@@ -1,5 +1,5 @@
 const ThemeBtn = () => {
-  return <div className="w-9 h-9 bg-[#F6F6F7] rounded-md cursor-pointer"></div>;
+  return <div className="w-9 h-9 bg-[#F6F6F7] dark:bg-[#333] rounded-md cursor-pointer"></div>;
 };
 
 export default ThemeBtn;
