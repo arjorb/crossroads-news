ffb899ac8d3dd3a2b9dfa2720b3d9cf9d4786ab0
John U <arjorb@gmail.com>
2023-04-19 11:15:16 +0200

    adding the themeBtn Icons both BiSun and BiMoon

--- src/components/layout/ThemeBtn.jsx
+++ src/components/layout/ThemeBtn.jsx
@@ -1,5 +1,10 @@
+import { BiSun, BiMoon } from "react-icons/bi";
 const ThemeBtn = () => {
-  return <div className="w-9 h-9 bg-[#F6F6F7] dark:bg-[#333] rounded-md cursor-pointer"></div>;
+  return (
+    <div className="w-9 h-9 bg-[#F6F6F7] dark:bg-[#333] dark:hover:bg-[#444] rounded-md cursor-pointer flex items-center justify-center">
+      <BiSun size={20} className="text-[#eaeaec]" />
+    </div>
+  );
 };
 
 export default ThemeBtn;
