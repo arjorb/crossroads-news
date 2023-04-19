55faf0cf9687b350f53735a06a4411a341d2d1da
John U <arjorb@gmail.com>
2023-04-19 11:19:21 +0200

    adding the darkMode class into the tailwind config file

--- src/components/layout/ThemeBtn.jsx
+++ src/components/layout/ThemeBtn.jsx
@@ -1,8 +1,8 @@
 import { BiSun, BiMoon } from "react-icons/bi";
 const ThemeBtn = () => {
   return (
-    <div className="w-9 h-9 bg-[#F6F6F7] dark:bg-[#333] dark:hover:bg-[#444] rounded-md cursor-pointer flex items-center justify-center">
-      <BiSun size={20} className="text-[#eaeaec]" />
+    <div className="w-9 h-9 bg-[#F6F6F7] hover:bg-[#e9e9e9] dark:bg-[#333] dark:hover:bg-[#444] rounded-md cursor-pointer flex items-center justify-center">
+      <BiSun size={20} className="text-[#222] dark:text-[#eaeaec]" />
     </div>
   );
 };
