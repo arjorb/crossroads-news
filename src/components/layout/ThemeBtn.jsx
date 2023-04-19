3cf74b08d8ba11251ef70b40661234b8c2a1151b
John U <arjorb@gmail.com>
2023-04-19 11:45:23 +0200

    done working on the dark theme

--- src/components/layout/ThemeBtn.jsx
+++ src/components/layout/ThemeBtn.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
 import { BiSun, BiMoon } from "react-icons/bi";
 const ThemeBtn = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
-    <div className="w-9 h-9 bg-[#F6F6F7] hover:bg-[#e9e9e9] dark:bg-[#333] dark:hover:bg-[#444] rounded-md cursor-pointer flex items-center justify-center">
-      <BiSun size={20} className="text-[#222] dark:text-[#eaeaec]" />
+    <div
+      onClick={handleTheme}
+      className="w-9 h-9 bg-[#F6F6F7] hover:bg-[#e9e9e9] dark:bg-[#333] dark:hover:bg-[#444] rounded-md cursor-pointer flex items-center justify-center"
+    >
+      {theme === "dark" ? <BiSun size={20} className="text-[#eaeaec]" /> : <BiMoon size={20} className="text-[#333]" />}
     </div>
   );
 };
